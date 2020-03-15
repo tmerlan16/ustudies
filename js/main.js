0c83d2aac716f65a5db13b4ade0a57c13c180395
tmerlan16 <you@example.com>
2020-03-15 12:04:47 +0600

    first

---
+++ js/main.js
@@ -0,0 +1,42 @@
+///****BURGER-MEN****///
+
+
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+    let links = menu.find('.burger-menu_link');
+    let overlay = menu.find('.burger-menu_overlay');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu() {
+        menu.toggleClass('burger-menu_active');
+
+        if (menu.hasClass('burger-menu_active')) {
+            $('body').css('overlow', 'hidden');
+        } else {
+            $('body').css('overlow', 'visible');
+        }
+    }
+}
+
+burgerMenu('.burger-menu');
+
+
+///****BURGER-MEN****///
+
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+        $('#header__top').addClass('header-fixed');
+    } else {
+        $('#header__top').removeClass('header-fixed');
+    }
+});
+
